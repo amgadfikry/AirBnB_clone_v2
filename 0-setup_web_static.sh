3b260742ade1617e3edd28852ca370fb963485df
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-09-06 15:48:42 -0700

    fix script to prevent dublication

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,8 +6,13 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo "this is simple content" | tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+echo "this is simple content" | sudo tee /data/web_static/releases/test/index.html
+if [ -d /data/web_static/current ]; then
+	if [ -L /data/web_static/current ]; then
+		sudo rm /data/web_static/current
+	fi
+fi
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 content=$(cat << 'END HEREDOC'
 	server {
@@ -33,6 +38,9 @@ content=$(cat << 'END HEREDOC'
 END HEREDOC
 )
 echo -e "$content" >> text.txt
-sudo sed -i '/http {/r text.txt' /etc/nginx/nginx.conf
+check=$(grep -Fxf text.txt /etc/nginx/nginx.conf)
+if [ -z "$check" ]; then
+	sudo sed -i '/http {/r text.txt' /etc/nginx/nginx.conf
+fi
 sudo rm text.txt
 sudo service nginx reload
