a0e5ad32d959b0643fc1b29c2b67003de4816013
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-09-06 14:14:33 -0700

    bash script to sets up nginx configuration

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# bash script that sets up webstatic
+
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "this is simple content" | tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+content=$(cat << 'END HEREDOC'
+	server {
+		listen 80 default_server;
+		listen [::]:80 default_server;
+		root /var/www/html;	
+		index index.html index.htm index.nginx-debian.html;
+		server_name _;
+		rewrite ^/redirect_me https://www.youtube.com permanent;
+		add_header X-Served-By $hostname;
+		error_page 404 /custom_404.html;
+		location = /custom_404.html {
+			root /usr/share/nginx/html;
+			internal;
+		}
+		location / {
+			try_files $uri $uri/ =404;
+		}
+		location /hbnb_static/ {
+			alias /data/web_static/current/;
+		}
+	}
+END HEREDOC
+)
+echo -e "$content" >> text.txt
+sudo sed -i '/http {/r text.txt' /etc/nginx/nginx.conf
+sudo rm text.txt
+sudo service nginx reload
