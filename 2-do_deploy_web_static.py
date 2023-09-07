443a3bf65056d82e3963c3cc033021bd759c3ec8
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-09-06 18:58:30 -0700

    fabric script that move archive to server and deploy it

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""fabric svript deploy compress file """
+from fabric.api import *
+import os
+
+
+env.hosts = ["54.237.107.99", "100.25.103.38"]
+env.user = "ubuntu"
+
+
+def do_deploy(archive_path):
+    """function that deploy the archive file """
+    if not os.path.exists(archive_path):
+        return False
+    res = put(local_path=archive_path, remote_path="/tmp/")
+    if res.failed:
+        return False
+    file_name = archive_path.split("/")[-1]
+    remote_path = f"/tmp/{file_name}"
+    with cd("/data/web_static/releases/"):
+        res = sudo(f"tar -xzvf {remote_path}")
+        if res.failed:
+            return False
+    res = sudo(f"rm {remote_path}")
+    if res.failed:
+        return False
+    res = sudo("rm /data/web_static/current")
+    if res.failed:
+        return False
+    folder_old = "/data/web_static/releases/web_static"
+    folder_new = f"/data/web_static/releases/{file_name.split('.')[0]}"
+    res = sudo(f"mv {folder_old} {folder_new}")
+    if res.failed:
+        return False
+    res = sudo(f"ln -s {folder_new} /data/web_static/current")
+    if res.failed:
+        return False
+    return True
