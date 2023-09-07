f1b9764ce1b5925475b79adfc483e413e3466ea2
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-09-07 04:06:39 -0700

    add line to delete and recreate symbolic link

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,12 +6,7 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 echo "this is simple content" | sudo tee /data/web_static/releases/test/index.html
-if [ -d /data/web_static/current ]; then
-	if [ -L /data/web_static/current ]; then
-		sudo rm /data/web_static/current
-	fi
-fi
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 content=$(cat << 'END HEREDOC'
 	location /hbnb_static {
