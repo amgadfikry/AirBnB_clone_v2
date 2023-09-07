66b7e7de856e34a37b16d4a31e4454292142d450
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-09-06 17:55:30 -0700

    modify script to add hors to name

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,11 +7,10 @@ from fabric.api import local, lcd
 def do_pack():
     """ function that compress static web folder"""
     n = datetime.now()
-    name = f"web_static_{n.year}{n.month}{n.day}{n.minute}{n.second}.tgz"
+    t = f"web_static_{n.year}{n.month}{n.day}{n.hour}{n.minute}{n.second}.tgz"
     local("mkdir -p versions")
     with lcd("./versions"):
-        print("amgad")
-        res = local(f"tar -czvf {name} ../web_static")
+        res = local(f"tar -czvf {t} ../web_static")
         if res.succeeded:
-            return f"versions/{name}"
+            return f"versions/{t}"
     return None
