6a074ca7eb925ef03a74cb0865081e199e67eb82
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-09-07 05:58:47 -0700

    modify script

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -29,13 +29,14 @@ def do_deploy(archive_path):
         file_name = archive_path.split("/")[-1]
         t_path = f"/tmp/{file_name}"
         with cd("/data/web_static/releases/"):
-            res = sudo(f"tar -xzvf {t_path}")
+            res = sudo(f"tar -xzf {t_path}")
         sudo(f"rm {t_path}")
         sudo("rm -rf /data/web_static/current")
         folder_old = "/data/web_static/releases/web_static"
         folder_new = f"/data/web_static/releases/{file_name.split('.')[0]}"
         sudo(f"mv {folder_old} {folder_new}")
         sudo(f"ln -s {folder_new} /data/web_static/current")
+        print("New version deployed!")
         return True
     except Exception:
         return False
