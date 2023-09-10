b57ccfbcaa49f98c77d0f1eb22f12f83a8a08dbb
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-09-10 21:59:04 +0300

    fix puppet manifest

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,23 +1,18 @@
 # puppet manifest configure nginx
+package { 'nginx':
+  ensure => 'installed'
+}
 exec { 'run1':
-  command => 'sudo apt-get -y update',
-  path    => [ '/usr/local/bin/', '/usr/bin/', '/bin/' ]
-}
-exec { 'run2':
-  command => 'sudo apt-get -y install nginx',
-  path    => [ '/usr/local/bin/', '/usr/bin/', '/bin/' ]
-}
-exec { 'run3':
   command => 'sudo mkdir -p /data/web_static/shared/',
   path    => [ '/usr/local/bin/', '/usr/bin/', '/bin/' ]
 }
-exec { 'run4':
+exec { 'run2':
   command => 'sudo mkdir -p /data/web_static/releases/test/',
   path    => [ '/usr/local/bin/', '/usr/bin/', '/bin/' ]
 }
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'present',
-  content => 'this is simple content'
+exec { 'run3':
+  command => 'echo "this is simple content" | sudo tee /data/web_static/releases/test/index.html',
+  path    => [ '/usr/local/bin/', '/usr/bin/', '/bin/' ]
 }
 exec { 'run4':
   command => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
@@ -27,11 +22,10 @@ exec { 'change_permission':
   command => 'sudo chown -R ubuntu:ubuntu /data/',
   path    => [ '/usr/local/bin/', '/usr/bin/', '/bin/' ]
 }
-file_line { 'add_location':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => '\\\tlocation /hbnb_static {\n\\\t\\\talias /data/web_static/current/;\n\\\t}'
+$content = 'location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}'
+exec { 'config':
+  command => "sudo sed -i \"/server_name _;/a ${content}\" /etc/nginx/sites-available/default",
+  path    => [ '/usr/local/bin/', '/usr/bin/', '/bin/' ]
 }
 exec { 'restart':
   command => 'sudo service nginx restart',
