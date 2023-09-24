e1b13d851ada42030c24eb2206f8fd515a1c3e85
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-09-24 21:49:04 +0300

    create flask web app with route as number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" flask web application """
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    """ route home function """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ route hbnb function """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def text(text):
+    """ route text function """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', defaults={"text": "is cool"})
+@app.route('/python/<text>')
+def python(text):
+    """ route python function """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
